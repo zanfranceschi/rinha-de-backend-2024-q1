271159b01eba7f402602ec6fc8dad93fa54268d4
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2024-01-23 01:42:34 -0300

    :nail_care:

--- load-test/user-files/simulations/rinhabackend/RinhaBackendCrebitosSimulation.scala
+++ load-test/user-files/simulations/rinhabackend/RinhaBackendCrebitosSimulation.scala
@@ -144,19 +144,17 @@ class RinhaBackendCrebitosSimulation
   setUp(
     criteriosClientes.inject(
       atOnceUsers(1)
-    )
-    // Mais suave que isso, nem sei se tem.
-    .andThen(
+    ).andThen(
       debitos.inject(
-        rampUsersPerSec(1).to(220).during(1.minutes),
+        rampUsersPerSec(1).to(220).during(2.minutes),
         constantUsersPerSec(220).during(2.minutes)
       ),
       creditos.inject(
-        rampUsersPerSec(1).to(110).during(1.minutes),
+        rampUsersPerSec(1).to(110).during(2.minutes),
         constantUsersPerSec(110).during(2.minutes)
       ),
       extratos.inject(
-        rampUsersPerSec(1).to(10).during(1.minutes),
+        rampUsersPerSec(1).to(10).during(2.minutes),
         constantUsersPerSec(10).during(2.minutes)
       )
     )
