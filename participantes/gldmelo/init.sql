89a13f1adaa8f3ea8e5a4e4bf9488b56955227c9
Guilherme Melo <gldmelo@gmail.com>
2024-02-07 01:04:43 -0300

    alteração da imagem docker e portas do nginx

--- participantes/gldmelo/init.sql
+++ participantes/gldmelo/init.sql
@@ -27,11 +27,11 @@ DO $$
 BEGIN
 	INSERT INTO clientes (nome, limite)
 	VALUES
-		('o barato sai caro', 1000 * 100),
-		('zan corp ltda', 800 * 100),
-		('les cruders', 10000 * 100),
-		('padaria joia de cocaia', 100000 * 100),
-		('kid mais', 5000 * 100);
+		('Fulano', 1000 * 100),
+		('Beltrano', 800 * 100),
+		('Cicrano', 10000 * 100),
+		('Dionano', 100000 * 100),
+		('Eutano', 5000 * 100);
 	
 	INSERT INTO saldos (cliente_id, valor)
 		SELECT id, 0 FROM clientes;
