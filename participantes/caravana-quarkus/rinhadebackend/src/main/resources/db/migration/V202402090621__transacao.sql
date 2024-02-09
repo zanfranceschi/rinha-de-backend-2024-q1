a6377ff9e2ba8860701045adeb41bb70c3c1b595
Julio Faerman <jufaerma@redhat.com>
2024-02-09 14:00:13 +0100

    idented

--- participantes/caravana-quarkus/rinhadebackend/src/main/resources/db/migration/V202402090621__transacao.sql
+++ participantes/caravana-quarkus/rinhadebackend/src/main/resources/db/migration/V202402090621__transacao.sql
@@ -5,27 +5,30 @@ DECLARE
     v_saldo INT;
     v_limite INT;
 BEGIN
-	IF p_tipo = 'd' THEN
+    IF p_tipo = 'd' THEN
         diff := p_valor * -1;
     ELSE
         diff := p_valor;
     END IF;
 
-	SELECT saldo, limite INTO v_saldo, v_limite FROM clientes WHERE id = p_cliente_id;
+	SELECT saldo, limite 
+        INTO v_saldo, v_limite 
+        FROM clientes WHERE id = p_cliente_id;
 
-	IF v_saldo + diff < -1 * v_limite THEN
+    IF v_saldo + diff < -1 * v_limite THEN
         RAISE 'LIMITE_INDISPONIVEL [%, %, %]', v_saldo, diff, v_limite;
     END IF;
 
-	UPDATE clientes SET saldo = saldo + diff WHERE id = p_cliente_id;
+	UPDATE clientes 
+        SET saldo = saldo + diff 
+        WHERE id = p_cliente_id;
 
 	INSERT INTO transacoes (cliente_id, valor, tipo, descricao)
-	VALUES (p_cliente_id, p_valor, p_tipo, p_descricao);
+	    VALUES (p_cliente_id, p_valor, p_tipo, p_descricao);
 	
 	RETURN;
 EXCEPTION
     WHEN OTHERS THEN
-        -- An error occurred, so rollback the transaction
         RAISE 'Error processing transaction: %', SQLERRM;
         ROLLBACK;
 END;
