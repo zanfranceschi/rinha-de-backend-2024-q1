28a883ffc6a58df3599a3d0e3547136f5b90b7f0
Lucas <lucasmula@gmail.com>
2024-02-15 18:46:37 +0000

    lidando com concorrencia

--- participantes/lucas-hsm/script.sql
+++ participantes/lucas-hsm/script.sql
@@ -12,15 +12,70 @@ create table transacoes (
     valor int
 );
 
-create index clientes_index on clientes using hash(id);
-create index transacoes_index on transacoes using hash(id_cliente);
+create index clientes_index on clientes using hash (id);
+create index transacoes_index on transacoes using hash (id_cliente);
 
-create procedure update_client(i int, s int, t char, d varchar(10), r timestamp with time zone, v int)
-       language SQL
-       begin atomic
-       update clientes set saldo = s where id = i;
-       insert into transacoes values (i, t, d, r, v);
-       end;
+CREATE OR REPLACE FUNCTION update_client(client_id int, val int, tipo char, descricao varchar(10), re timestamp with time zone)
+RETURNS TABLE (
+    new_limite int,
+    new_saldo int
+)
+LANGUAGE plpgsql AS $$
+DECLARE
+    csaldo int;
+    climite int;
+BEGIN
+    BEGIN
+        SELECT saldo, limite INTO csaldo, climite FROM clientes WHERE id = client_id FOR UPDATE;
+
+        IF (csaldo - val) < (climite * -1) THEN
+            RETURN QUERY SELECT -1, -1;
+            RETURN;
+        END IF;
+
+        UPDATE clientes SET saldo = (csaldo - val) WHERE id = client_id;
+
+        INSERT INTO transacoes (id_cliente, tipo, descricao, realizada_em, valor)
+        VALUES (client_id, tipo, descricao, re, ABS(val));
+    END;
+    RETURN QUERY SELECT climite, (csaldo - val);
+    RETURN;
+END;
+$$;
+
+CREATE TYPE Transacao AS (
+       tipo char,
+       descricao varchar(10),
+       valor int,
+       realizada_em TIMESTAMP WITH TIME ZONE
+);
+
+CREATE OR REPLACE FUNCTION get_client_and_transactions(client_id INT)
+RETURNS TABLE (
+    nlimite int,
+    nsaldo int,
+    ntipo char,
+    ndescricao varchar(10),
+    nvalor int,
+    nrealizada_em TIMESTAMP WITH TIME ZONE
+)
+AS $$
+DECLARE
+    ntransacoes Transacao [];
+    transacao record;
+    c int;
+BEGIN
+    SELECT limite, saldo INTO nlimite, nsaldo FROM clientes WHERE id = client_id;
+    SELECT COUNT(id_cliente) INTO c FROM transacoes where id_cliente = client_id;
+    IF c < 1 then
+        RETURN QUERY SELECT nlimite, nsaldo, ntipo, ndescricao, nvalor, nrealizada_em;
+        RETURN;
+    END IF;
+    FOR transacao IN SELECT tipo, descricao, valor, realizada_em FROM transacoes where id_cliente = client_id ORDER BY realizada_em DESC LIMIT 10 LOOP
+        RETURN QUERY SELECT nlimite, nsaldo, transacao.tipo, transacao.descricao, transacao.valor, transacao.realizada_em;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 
 DO $$
 BEGIN
