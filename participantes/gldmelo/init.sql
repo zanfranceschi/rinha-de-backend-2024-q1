047e11fa88b54c50c05ff58028423d613122a360
Guilherme Melo <gldmelo@gmail.com>
2024-02-15 15:24:51 -0300

    Atualização com versão funcional publicada.

--- participantes/gldmelo/init.sql
+++ participantes/gldmelo/init.sql
@@ -1,26 +1,19 @@
-CREATE TABLE clientes (
+CREATE UNLOGGED TABLE clientes (
 	id SERIAL PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
-	limite INTEGER NOT NULL
+	limite INTEGER NOT NULL,
+	saldo INTEGER NOT NULL DEFAULT 0,
+	CONSTRAINT valida_saldo CHECK (saldo >= (- limite))
 );
 
-CREATE TABLE transacoes (
+CREATE UNLOGGED TABLE transacoes (
 	id SERIAL PRIMARY KEY,
 	cliente_id INTEGER NOT NULL,
 	valor INTEGER NOT NULL,
 	tipo CHAR(1) NOT NULL,
 	descricao VARCHAR(10) NOT NULL,
 	realizada_em TIMESTAMP NOT NULL DEFAULT NOW(),
-	CONSTRAINT fk_clientes_transacoes_id
-		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
-);
-
-CREATE TABLE saldos (
-	id SERIAL PRIMARY KEY,
-	cliente_id INTEGER NOT NULL,
-	valor INTEGER NOT NULL,
-	CONSTRAINT fk_clientes_saldos_id
-		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+	CONSTRAINT fk_clientes_transacoes_id FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
 DO $$
@@ -30,10 +23,41 @@ BEGIN
 		('Fulano', 1000 * 100),
 		('Beltrano', 800 * 100),
 		('Cicrano', 10000 * 100),
-		('Dionano', 100000 * 100),
-		('Eutano', 5000 * 100);
-	
-	INSERT INTO saldos (cliente_id, valor)
-		SELECT id, 0 FROM clientes;
+		('Deutrano', 100000 * 100),
+		('Eutrano', 5000 * 100);
 END;
 $$;
+
+CREATE OR REPLACE FUNCTION inserir_transacao_credito_e_retornar_saldo (
+	clienteid_in int,
+	valor_in int,
+	descricao_in varchar(10)
+)
+RETURNS int
+AS $$
+	DECLARE saldo_atualizado int;
+BEGIN	
+	INSERT INTO "transacoes" ("cliente_id", "valor", "tipo", "descricao") values (clienteid_in, valor_in, 'c', descricao_in);
+	UPDATE "clientes" set "saldo" = "saldo" + valor_in where "id" = clienteid_in RETURNING "saldo" into saldo_atualizado;
+    return saldo_atualizado;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_transacao_debito_e_retornar_saldo (
+	clienteid_in int,
+	valor_in int,
+	descricao_in varchar(10)
+)
+RETURNS int
+AS $$
+	DECLARE saldo_atualizado int;
+BEGIN	
+    UPDATE "clientes" set "saldo" = "saldo" - valor_in where "id" = clienteid_in and "saldo" - valor_in >= ("limite" * -1) returning "saldo" into saldo_atualizado;
+
+    IF saldo_atualizado IS NOT NULL THEN
+	    INSERT INTO "transacoes" ("cliente_id", "valor", "tipo", "descricao") values (clienteid_in, valor_in, 'd', descricao_in);
+    END IF;
+
+    RETURN saldo_atualizado;
+END;
+$$ LANGUAGE plpgsql;
