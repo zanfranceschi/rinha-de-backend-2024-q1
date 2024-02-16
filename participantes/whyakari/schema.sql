3d6831dac7ea1338f9ccc84893231305dabd30af
akari <akariondev@gmail.com>
2024-02-16 21:47:33 +0000

    update all files
    Signed-off-by: akari <akariondev@gmail.com>

--- participantes/whyakari/schema.sql
+++ participantes/whyakari/schema.sql
@@ -1,23 +1,23 @@
-CREATE TABLE IF NOT EXISTS "clientes" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "saldo" INTEGER NOT NULL,
-    "limite" INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+    saldo INT NOT NULL,
+    limite INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "transacoes" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "valor" INTEGER NOT NULL,
-    "id_cliente" INTEGER NOT NULL,
-    "tipo" TEXT NOT NULL, -- Altere para TEXT
-    "descricao" VARCHAR(10) NOT NULL,
-    "realizada_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY ("id_cliente") REFERENCES "clientes"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS transacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    valor INT NOT NULL,
+    id_cliente INT NOT NULL,
+    tipo CHAR(1) NOT NULL,
+    descricao VARCHAR(10) NOT NULL,
+    realizada_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-INSERT INTO clientes (saldo, limite)
-VALUES
-    (0, 100000),
-    (0, 80000),
-    (0, 1000000),
-    (0, 10000000),
-    (0, 500000);
+INSERT INTO clientes (nome, saldo, limite) VALUES
+    ('Akari', 100000, 0),
+    ('Isabella', 80000, 0),
+    ('Julia', 1000000, 0),
+    ('Hendrick', 10000000, 0),
+    ('Gustavo', 500000, 0);
