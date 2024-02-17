73686ef3a96712e5d7e9ca87fea9e1898251491f
Gabriel <gabriel.fonsecadev@gmail.com>
2024-02-16 23:31:17 -0300

    rinha gbfonseca

--- participantes/gbfonseca/mongo-init.js
+++ participantes/gbfonseca/mongo-init.js
@@ -3,8 +3,8 @@ db = conn.getDB("rinha");
 
 db.clientes.createIndex({ id: 1 }, { unique: true });
 
-db.clientes.insert({ saldo_inicial: 0, limite: 1000 * 100, id: 1 });
-db.clientes.insert({ saldo_inicial: 0, limite: 800 * 100, id: 2 });
-db.clientes.insert({ saldo_inicial: 0, limite: 10000 * 100, id: 3 });
-db.clientes.insert({ saldo_inicial: 0, limite: 100000 * 100, id: 4 });
-db.clientes.insert({ saldo_inicial: 0, limite: 5000 * 100, id: 5 });
+db.clientes.insert({ saldo: 0, limite: 1000 * 100, id: 1 });
+db.clientes.insert({ saldo: 0, limite: 800 * 100, id: 2 });
+db.clientes.insert({ saldo: 0, limite: 10000 * 100, id: 3 });
+db.clientes.insert({ saldo: 0, limite: 100000 * 100, id: 4 });
+db.clientes.insert({ saldo: 0, limite: 5000 * 100, id: 5 });
