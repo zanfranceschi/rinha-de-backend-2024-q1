2d11b46414865a12bb0c82a680000ba5c1c9a5e0
rinha-robotica <rinha@rinhadebackend.org>
2024-02-17 01:54:23 +0000

    remoção do script quantas faltam?

--- quantos-testes-faltam?.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-pushd ./participantes > /dev/null
-echo "APIs to be tested:"
-find '.' -mindepth 1 -maxdepth 1 -type d \! -exec test -e '{}/testada' \; -print
-find '.' -mindepth 1 -maxdepth 1 -type d \! -exec test -e '{}/testada' \; -print | wc -l
-popd > /dev/null
