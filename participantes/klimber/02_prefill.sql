6a2d4f36df2e119d06a012dec5f68661a7ada6d8
Allan Krueger <allankrg@amazon.com>
2024-02-18 09:12:39 -0300

    Submissão Klimber

---
+++ participantes/klimber/02_prefill.sql
@@ -0,0 +1,11 @@
+insert into Cliente (id, limite) values (1, 100000),
+                                        (2, 80000),
+                                        (3, 1000000),
+                                        (4, 10000000),
+                                        (5, 500000);
+
+insert into Saldo (id, cliente_id, saldo) values (1, 1, 0),
+                                                 (2, 2, 0),
+                                                 (3, 3, 0),
+                                                 (4, 4, 0),
+                                                 (5, 5, 0);
