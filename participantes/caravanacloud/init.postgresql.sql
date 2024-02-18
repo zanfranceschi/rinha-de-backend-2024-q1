d084fd13f2efa905dc3e37dc5b875dc7a4b88bca
Julio Faerman <jufaerma@redhat.com>
2024-02-18 13:56:51 +0100

    host mode for the win

--- participantes/caravanacloud/init.postgresql.sql
+++ participantes/caravanacloud/init.postgresql.sql
@@ -27,21 +27,26 @@ CREATE TYPE transacao_result AS (saldo INT, limite INT);
 CREATE OR REPLACE FUNCTION proc_transacao(p_cliente_id INT, p_valor INT, p_tipo VARCHAR, p_descricao VARCHAR)
 RETURNS transacao_result as $$
 DECLARE
+    diff INT;
     v_saldo INT;
     v_limite INT;
     result transacao_result;
 BEGIN
+    IF p_tipo = 'd' THEN
+        diff := p_valor * -1;
+    ELSE
+        diff := p_valor;
+    END IF;
+
     PERFORM * FROM clientes WHERE id = p_cliente_id FOR UPDATE;
 
-    UPDATE clientes 
-    SET saldo = CASE 
-                    WHEN p_tipo = 'd' THEN saldo - p_valor
-                    ELSE saldo + p_valor
-                END
-    WHERE id = p_cliente_id
-    RETURNING saldo, limite INTO v_saldo, v_limite;
 
-    IF (v_saldo) < (-1 * v_limite) THEN
+    UPDATE clientes 
+        SET saldo = saldo + diff 
+        WHERE id = p_cliente_id
+        RETURNING saldo, limite INTO v_saldo, v_limite;
+
+    IF (v_saldo + diff) < (-1 * v_limite) THEN
         RAISE 'LIMITE_INDISPONIVEL [%, %, %]', v_saldo, diff, v_limite;
     ELSE
         result := (v_saldo, v_limite)::transacao_result;
