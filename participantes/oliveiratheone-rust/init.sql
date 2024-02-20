c91cc2718b5b5cb36aada3fb421d6c635f014987
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-02-20 13:52:48 -0300

    Atualiza imagem & Testar novamente oliveiratheone

--- participantes/oliveiratheone-rust/init.sql
+++ participantes/oliveiratheone-rust/init.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 CREATE UNLOGGED TABLE
     "clientes" (
                    "id" SERIAL NOT NULL,
@@ -15,13 +13,41 @@ CREATE UNLOGGED TABLE
                      "id_cliente" INTEGER NOT NULL,
                      "tipo" char(1) NOT NULL,
                      "descricao" VARCHAR(10) NOT NULL,
-                     "realizada_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                     "realizada_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                      CONSTRAINT "transacoes_pkey" PRIMARY KEY ("id")
 );
 
+CREATE OR REPLACE FUNCTION get_extrato(customer_id INT)
+    RETURNS JSON AS
+$$
+DECLARE
+customer_data JSON;
+    statements_data JSON;
+BEGIN
+SELECT json_build_object('total', saldo, 'limite', limite, 'data_extrato', now())
+INTO customer_data
+FROM clientes
+WHERE id = customer_id;
+
+SELECT COALESCE(json_agg(json_build_object('valor', valor, 'tipo', tipo, 'descricao', descricao, 'realizada_em', realizada_em)), '[]'::JSON)
+INTO statements_data
+FROM (
+         SELECT valor, tipo, descricao, realizada_em
+         FROM transacoes
+         WHERE id_cliente = customer_id
+         ORDER BY realizada_em DESC
+             LIMIT 10
+     ) AS t;
+
+RETURN json_build_object(
+        'saldo', customer_data,
+        'ultimas_transacoes', statements_data
+       );
+END;
+$$
+LANGUAGE plpgsql;
+
 CREATE INDEX transacoes_ordering ON transacoes (realizada_em DESC, id_cliente);
-CREATE INDEX idx_clientes_id ON clientes (id);
-CREATE INDEX idx_clientes_limite ON clientes (limite);
 
 INSERT INTO
     clientes (saldo, limite)
