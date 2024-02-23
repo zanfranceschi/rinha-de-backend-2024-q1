b063737494a0b19a2e5285584b9a699066829ac5
santannaf <thales.farias.santanna@gmail.com>
2024-02-22 22:56:35 -0300

    upgrading code and retest

--- participantes/santannaf_web/init.sql
+++ participantes/santannaf_web/init.sql
@@ -14,16 +14,16 @@ values (1, 1000 * 100),
 
 create table transactions
 (
-    id          varchar(40) not null,
-    customer_id integer     not null,
-    type        varchar(1)  not null,
-    amount      integer     not null,
-    description varchar(10) not null,
-    created_at  timestamp   not null
+    id            varchar(40) not null,
+    customer_id   integer     not null,
+    type          varchar(1)  not null,
+    amount        integer     not null,
+    description   varchar(10) not null,
+    created_at    timestamp   not null,
+    account_limit integer     not null,
+    balance       integer     not null
 );
 
-create index if not exists index_transaction_customer_id on transactions using btree (customer_id);
-create index if not exists index_customers_customer_id on account using btree (customer_id);
-create index if not exists index_transaction_created_at on transactions (created_at desc);
+create index index_transaction_statement on transactions (created_at, customer_id);
 
 grant all privileges on database db_financial to admin;
