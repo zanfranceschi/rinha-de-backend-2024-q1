edb7dc6ca083503261cb0098d8f7a0834499aa8e
AlcivanLucas <alcivan14lucas@gmail.com>
2024-02-27 16:09:02 -0300

    api pronta, começando fase de testes

--- participantes/AlcivanLucas/api_node/db/script.sql
+++ participantes/AlcivanLucas/api_node/db/script.sql
@@ -2,19 +2,41 @@
 -- CREATE DATABASE IF NOT EXISTS rinha;
 -- USE rinha;
 
-CREATE TABLE IF NOT EXISTS clientes(
-    id SERIAL PRIMARY KEY ,
-    limite INTEGER NOT NULL,  -- deve ser o limite cadastrado do cliente. 
-    saldo INTEGER NOT NULL -- deve ser o novo saldo após a conclusão da transação.
+-- CREATE TABLE IF NOT EXISTS clientes(
+--     id SERIAL PRIMARY KEY ,
+--     limite INTEGER NOT NULL,  -- deve ser o limite cadastrado do cliente. 
+--     saldo INTEGER NOT NULL -- deve ser o novo saldo após a conclusão da transação.
+-- );
+
+-- CREATE TABLE IF NOT EXISTS transacoes(
+--     id SERIAL PRIMARY KEY,
+--     id_cliente INTEGER NOT NULL REFERENCES clientes, 
+--     valor INTEGER NOT NULL, -- deve ser o valor da transação.
+--     tipo CHAR(1) NOT NULL, -- deve ser c para crédito e d para débito.
+--     descricao VARCHAR(10) NOT NULL,
+--     realizada_em TIMESTAMP NOT NULL -- deve ser a data/hora da realização da transação.
+-- );
+
+
+-- CreateTable
+CREATE TABLE "clientes" (
+    "id" SERIAL NOT NULL,
+    "limite" INTEGER NOT NULL,
+    "saldo" INTEGER NOT NULL,
+
+    CONSTRAINT "clientes_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE IF NOT EXISTS transacoes(
-    id SERIAL PRIMARY KEY,
-    id_cliente INTEGER NOT NULL REFERENCES clientes, 
-    valor INTEGER NOT NULL, -- deve ser o valor da transação.
-    tipo CHAR(1) NOT NULL, -- deve ser c para crédito e d para débito.
-    descricao VARCHAR(10) NOT NULL,
-    realizada_em TIMESTAMP NOT NULL -- deve ser a data/hora da realização da transação.
+-- CreateTable
+CREATE TABLE "transacoes" (
+    "id" SERIAL NOT NULL,
+    "cliente_id" INTEGER NOT NULL,
+    "valor" INTEGER NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "realizada_em" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "transacoes_pkey" PRIMARY KEY ("id")
 );
 
 INSERT INTO clientes (limite, saldo) VALUES (100000, 0);
