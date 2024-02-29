a6136d4db3a4fdec06bed1e2be91394a0e3c70d8
thiper01 <57779605+thiper01@users.noreply.github.com>
2024-02-29 00:24:55 -0300

    Adequando às regras do pull request

--- participantes/thiper01-django/app/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import transacoes, extrato
-
-urlpatterns = [
-    path("<int:id>/transacoes", transacoes),
-    path("<int:id>/extrato", extrato),
-]
--- participantes/thiper01-django/app/test.py
+++
@@ -1,4 +0,0 @@
-# test.py
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
