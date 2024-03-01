d6f6334319015ccdf489b8d3826c4614604b451d
Renato L. F. Cunha <renatocunha@acm.org>
2024-03-01 14:48:22 -0300

    network_mode: host

--- executar-teste-local.sh
+++ executar-teste-local.sh
@@ -3,7 +3,7 @@
 # Use este script para executar testes locais
 
 RESULTS_WORKSPACE="$(pwd)/load-test/user-files/results"
-GATLING_BIN_DIR=$HOME/gatling/bin
+GATLING_BIN_DIR=$HOME/tmp/gatling/bin
 GATLING_WORKSPACE="$(pwd)/load-test/user-files"
 
 runGatling() {
