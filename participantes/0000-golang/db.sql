f4db138ae802c64e669a226264a1b541eb3ad600
wesleycremonini <wesleycremonini0@gmail.com>
2024-03-04 01:14:31 -0300

    .

--- participantes/0000-golang/db.sql
+++ participantes/0000-golang/db.sql
@@ -19,8 +19,8 @@ CREATE UNLOGGED TABLE transactions (
     customer_id SMALLINT NOT NULL,
     amount INTEGER NOT NULL,
     type CHAR(1) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    description VARCHAR(10) NOT NULL
+    description VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE
@@ -29,3 +29,66 @@ SET
   (autovacuum_enabled = false);
 
 CREATE INDEX idx_transactions ON transactions (customer_id asc);
+
+CREATE OR REPLACE FUNCTION debit(
+	customer_id_tx SMALLINT,
+	amount_tx INT,
+	description_tx VARCHAR(10))
+RETURNS TABLE (
+	new_balance INT,
+	success BOOL,
+	current_limit INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	current_balance int;
+	current_limit_amount int;
+BEGIN
+	PERFORM pg_advisory_xact_lock(customer_id_tx);
+
+	SELECT 
+		"limit",
+		balance
+	INTO
+		current_limit_amount,
+		current_balance
+	FROM customers
+	WHERE id = customer_id_tx;
+
+	IF current_balance - amount_tx >= current_limit_amount * -1 THEN
+		INSERT INTO transactions VALUES(DEFAULT, customer_id_tx, amount_tx, 'd', description_tx);
+		
+		RETURN QUERY
+    UPDATE customers 
+    SET balance = balance - amount_tx 
+    WHERE id = customer_id_tx
+    RETURNING balance, TRUE, "limit";
+
+	ELSE
+		RETURN QUERY SELECT current_balance, FALSE, current_limit_amount;
+	END IF;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION credit(
+	customer_id_tx SMALLINT,
+	amount_tx INT,
+	description_tx VARCHAR(10))
+RETURNS TABLE (
+	new_balance INT,
+	success BOOL,
+	current_limit INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	PERFORM pg_advisory_xact_lock(customer_id_tx);
+
+	INSERT INTO transactions VALUES(DEFAULT, customer_id_tx, amount_tx, 'c', description_tx);
+
+	RETURN QUERY
+		UPDATE customers
+		SET balance = balance + amount_tx
+		WHERE id = customer_id_tx
+		RETURNING balance, TRUE, "limit";
+END;
+$$;
