ff94567db91d472857c8f8ec8d80c64cd9cc8998
kleytonsolinho <kleytonsolinho@gmail.com>
2024-03-08 12:12:22 -0300

    fix: docker-compose

--- participantes/kleytonsolinho-golang/script.sql
+++ participantes/kleytonsolinho-golang/script.sql
@@ -19,14 +19,3 @@ INSERT INTO clientes (nome, limite, saldo)
     ('les cruders', 10000 * 100, 0),
     ('padaria joia de cocaia', 100000 * 100, 0),
     ('kid mais', 5000 * 100, 0);
-
-DO $$
-BEGIN
-  INSERT INTO clientes (nome, limite, saldo)
-  VALUES
-    ('o barato sai caro', 1000 * 100, 0),
-    ('zan corp ltda', 800 * 100, 0),
-    ('les cruders', 10000 * 100, 0),
-    ('padaria joia de cocaia', 100000 * 100, 0),
-    ('kid mais', 5000 * 100, 0);
-END; $$
