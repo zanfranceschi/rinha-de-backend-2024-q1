fc9c3218fb69751aa0c9e9433ab907e966f73e59
Danilo Siqueira <danilo@geodados.com.br>
2024-03-08 21:44:10 -0300

    Criação do docker-compose

---
+++ participantes/danilosiqueira/script.sql
@@ -0,0 +1,17 @@
+-- Coloque scripts iniciais aqui
+CREATE TABLE clientes (
+    id serial not null primary key,
+    nome varchar(100) not null,
+    limite decimal not null
+);
+
+DO $$
+BEGIN
+  INSERT INTO clientes (nome, limite)
+  VALUES
+    ('o barato sai caro', 1000 * 100),
+    ('zan corp ltda', 800 * 100),
+    ('les cruders', 10000 * 100),
+    ('padaria joia de cocaia', 100000 * 100),
+    ('kid mais', 5000 * 100);
+END; $$
