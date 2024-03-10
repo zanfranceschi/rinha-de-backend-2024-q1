e2fec75f23a69ce6858e054ea6815a6ba959e1d8
Giovanni Cornachini <giovanni.cornachini@nubank.com.br>
2024-03-10 19:30:47 -0300

    Update sql

--- participantes/gicornachini/init.sql
+++ participantes/gicornachini/init.sql
@@ -1,116 +1,100 @@
-CREATE UNLOGGED TABLE clientes (
-	id SERIAL PRIMARY KEY,
-	nome VARCHAR(50) NOT NULL,
-	limite INTEGER NOT NULL
+CREATE UNLOGGED TABLE IF NOT EXISTS "clientes" (
+	"id" SERIAL NOT NULL PRIMARY KEY,
+	"limite" INTEGER NOT NULL,
+	"saldo" BIGINT NOT NULL DEFAULT 0
 );
 
-CREATE UNLOGGED TABLE transacoes (
-	id SERIAL PRIMARY KEY,
-	cliente_id INTEGER NOT NULL,
-	valor INTEGER NOT NULL,
-	tipo CHAR(1) NOT NULL,
-	descricao VARCHAR(10) NOT NULL,
-	realizada_em TIMESTAMP NOT NULL DEFAULT NOW(),
-	CONSTRAINT fk_clientes_transacoes_id
-		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+CREATE UNLOGGED TABLE IF NOT EXISTS "transacoes" (
+	"id" SERIAL NOT NULL PRIMARY KEY,
+	"cliente_id" INTEGER NOT NULL,
+	"tipo" CHARACTER(1) NOT NULL,
+	"valor" BIGINT NOT NULL,
+	"descricao" VARCHAR(10) NOT NULL,
+	"realizada_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT "fk_clientes_transacoes_id" FOREIGN KEY ("cliente_id") REFERENCES clientes ("id")
 );
 
-CREATE UNLOGGED TABLE saldos (
-	id SERIAL PRIMARY KEY,
-	cliente_id INTEGER NOT NULL,
-	valor INTEGER NOT NULL,
-	CONSTRAINT fk_clientes_saldos_id
-		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
-);
+CREATE INDEX "fk_transacoes_cliente_id" ON "public"."transacoes" ("cliente_id");
 
-DO $$
-BEGIN
-	INSERT INTO clientes (nome, limite)
-	VALUES
-		('o barato sai caro', 1000 * 100),
-		('zan corp ltda', 800 * 100),
-		('les cruders', 10000 * 100),
-		('padaria joia de cocaia', 100000 * 100),
-		('kid mais', 5000 * 100);
-	
-	INSERT INTO saldos (cliente_id, valor)
-		SELECT id, 0 FROM clientes;
+
+INSERT INTO
+	clientes ("limite", "saldo")
+VALUES
+	(1000 * 100, 0),
+	(800 * 100, 0),
+	(10000 * 100, 0),
+	(100000 * 100, 0),
+	(5000 * 100, 0);
+
 END;
-$$;
 
-CREATE OR REPLACE FUNCTION debitar(
-	cliente_id_tx INT,
-	valor_tx INT,
-	descricao_tx VARCHAR(10))
-RETURNS TABLE (
-	novo_saldo INT,
-	possui_erro BOOL,
-	mensagem VARCHAR(20))
-LANGUAGE plpgsql
-AS $$
-DECLARE
-	saldo_atual int;
-	limite_atual int;
+CREATE OR REPLACE FUNCTION transacao(
+    _cliente_id INTEGER,
+    _valor INTEGER,
+    _tipo CHAR,
+    _descricao VARCHAR(10),
+    OUT codigo_erro SMALLINT,
+    OUT out_limite INTEGER,
+	OUT out_saldo BIGINT
+)
+RETURNS record AS
+$$
 BEGIN
-	PERFORM pg_advisory_xact_lock(cliente_id_tx);
-	SELECT 
-		c.limite,
-		COALESCE(s.valor, 0)
-	INTO
-		limite_atual,
-		saldo_atual
-	FROM clientes c
-		LEFT JOIN saldos s
-			ON c.id = s.cliente_id
-	WHERE c.id = cliente_id_tx;
+        IF _tipo = 'c' THEN
+            UPDATE clientes 
+            SET saldo = saldo + _valor 
+            WHERE id = _cliente_id 
+            RETURNING limite, saldo INTO out_limite, out_saldo;
+            INSERT INTO transacoes(cliente_id, valor, tipo, descricao)
+            VALUES (_cliente_id, _valor, _tipo, _descricao);
+            codigo_erro := 0;
+            RETURN;
+        ELSIF _tipo = 'd' THEN
+            UPDATE clientes
+            SET saldo = saldo - _valor
+            WHERE id = _cliente_id AND saldo - _valor > -limite
+            RETURNING limite, saldo INTO out_limite, out_saldo;
+            
+            IF FOUND THEN 
+              INSERT INTO transacoes(cliente_id, valor, tipo, descricao)
+              VALUES (_cliente_id, _valor, _tipo, _descricao);
+              codigo_erro := 0;
+            ELSE 
+              codigo_erro := 2;
+            END IF;
 
-	IF saldo_atual - valor_tx >= limite_atual * -1 THEN
-		INSERT INTO transacoes
-			VALUES(DEFAULT, cliente_id_tx, valor_tx, 'd', descricao_tx, NOW());
-		
-		UPDATE saldos
-		SET valor = valor - valor_tx
-		WHERE cliente_id = cliente_id_tx;
+            RETURN;
+        ELSE
+            codigo_erro := 3;
+            RETURN;
+        END IF;
+END;
+$$
+LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION extrato(
+    _cliente_id INTEGER
+)
+RETURNS table (
+	cliente_limite INTEGER,
+	cliente_saldo BIGINT,
+	id int,
+	cliente_id INTEGER,
+	tipo CHARACTER,
+	valor BIGINT,
+	descricao VARCHAR(10),
+	realizada_em TIMESTAMP
+	) 
+	AS
+$$
+BEGIN
 		RETURN QUERY
-			SELECT
-				valor,
-				FALSE,
-				'ok'::VARCHAR(20)
-			FROM saldos
-			WHERE cliente_id = cliente_id_tx;
-	ELSE
-		RETURN QUERY
-			SELECT
-				valor,
-				TRUE,
-				'saldo insuficente'::VARCHAR(20)
-			FROM saldos
-			WHERE cliente_id = cliente_id_tx;
-	END IF;
+		SELECT c.limite as "cliente_limite", c.saldo as "cliente_saldo", t.*
+		FROM clientes as c
+		LEFT JOIN transacoes as t ON c.id = t.cliente_id
+		WHERE c.id = _cliente_id
+		ORDER BY t.realizada_em DESC
+		LIMIT 10;
 END;
-$$;
-
-CREATE OR REPLACE FUNCTION creditar(
-	cliente_id_tx INT,
-	valor_tx INT,
-	descricao_tx VARCHAR(10))
-RETURNS TABLE (
-	novo_saldo INT,
-	possui_erro BOOL,
-	mensagem VARCHAR(20))
-LANGUAGE plpgsql
-AS $$
-BEGIN
-	PERFORM pg_advisory_xact_lock(cliente_id_tx);
-
-	INSERT INTO transacoes
-		VALUES(DEFAULT, cliente_id_tx, valor_tx, 'c', descricao_tx, NOW());
-
-	RETURN QUERY
-		UPDATE saldos
-		SET valor = valor + valor_tx
-		WHERE cliente_id = cliente_id_tx
-		RETURNING valor, FALSE, 'ok'::VARCHAR(20);
-END;
-$$;
+$$
+LANGUAGE plpgsql;
