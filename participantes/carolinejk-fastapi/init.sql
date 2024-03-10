57f7a46eaad1b1509059bb66b0105732d9b9d607
Caroline Nunes <53407022+Carolinewk@users.noreply.github.com>
2024-03-10 20:52:40 -0300

    carolinejk-fastapi (#1606)
    * carolinejk-fastapi
    
    * Delete participantes/carolinejk-fastapi/Dockerfile
    
    ---------
    
    Co-authored-by: Francisco Zanfranceschi <zanfranceschi@gmail.com>

---
+++ participantes/carolinejk-fastapi/init.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY,
+  limite INTEGER,
+  saldo INTEGER
+);
+
+INSERT INTO clientes (id, limite, saldo)
+VALUES
+  (1, 100000, 0),
+  (2, 80000, 0),
+  (3, 1000000, 0),
+  (4, 10000000, 0),
+  (5, 500000, 0);
+
+COMMIT;
